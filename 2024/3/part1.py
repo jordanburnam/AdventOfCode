bba92734966bdbcbdbad5172ddca10f9e57130c4
Jordan Burnam <me@jordanburnam.com>
2024-12-04 08:27:01 -0800

    completed part 1

--- 2024/3/part1.py
+++ 2024/3/part1.py
@@ -1,6 +1,6 @@
 import os
 import logging
-
+import re
 
 
 logging.basicConfig(
@@ -14,15 +14,26 @@ logging.basicConfig(
 
 
 input_file_path='2024/3/input/data'
-input_file_path='2024/3/input/example'
+# input_file_path='2024/3/input/example'
 
 logging.info(f"Reading file at '{input_file_path}'")
 
 line_count=0
 
-
+input_string = ""
 with open(input_file_path, 'r') as input_file:
     for line in input_file:
         line_count+=1
+        input_string += line
 
-print(f"total_lines:{line_count}")
+pattern = r"mul\((\d+),(\d+)\)"
+matches = re.findall(pattern, input_string)
+
+calculated_total = 0
+for match in matches:
+    if len(match) != 2:
+        raise Exception(f"Oh shoot, I am dead .... goodbye cruel world")
+    product = int(match[0]) * int(match[1])
+    calculated_total += product
+        
+print(f"calculated_total:{calculated_total}")
